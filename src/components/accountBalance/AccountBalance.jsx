4e6d9d8b76a1170b395a4f76088eac5a9ff6e396
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-25 19:36:27 +0100

    Create AccountBalance styles

--- src/components/accountBalance/AccountBalance.jsx
+++ src/components/accountBalance/AccountBalance.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react'
+import './AccountBalance.css'
 const AccounBalance = () => {
   return(
     <div className="container">
       <div className="row align-items-center">
-        <div className="col-sm">
+        <div className="box col-sm shadow p-3 mb-5 bg-body rounded">
           <h4 className="title">Your account balance:</h4>
           <div className="amount">1500$</div>
           <button className="btn btn-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
         </div>
-        <div className="col-sm">
+        <div className="box col-sm shadow p-3 mb-5 bg-body rounded">
           <h4 className="title">Your saving balance:</h4>
           <div className="amount saving">1390$</div>
           <button className="btn btn-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
