74c1af84be4894bd6082131a5adcf8e4d4930ab2
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-02-25 17:59:37 +0100

    Add bootstrap to add some styles

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="container">
+    <div className="app">
       <section className="section">
         <AccounBalance />
       </section>
--- src/components/accountBalance/AccountBalance.jsx
+++ src/components/accountBalance/AccountBalance.jsx
@@ -1,19 +1,17 @@
 import React, { useState } from 'react'
 const AccounBalance = () => {
   return(
-    <div className="column">
-      <div className="column">
-        <div className="box">
-          <h4 className="title is-4">Your account balance:</h4>
+    <div className="container">
+      <div className="row align-items-center">
+        <div className="col-sm">
+          <h4 className="title">Your account balance:</h4>
           <div className="amount">1500$</div>
-          <button className="button is-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
+          <button className="btn btn-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
         </div>
-      </div>
-      <div className="column">
-        <div className="box">
-          <h4 className="title is-4">Your saving balance:</h4>
+        <div className="col-sm">
+          <h4 className="title">Your saving balance:</h4>
           <div className="amount saving">1390$</div>
-          <button className="button is-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
+          <button className="btn btn-info" onClick={()=>{console.log('Sending money');}}>Send 100$</button>
         </div>
       </div>
     </div>
